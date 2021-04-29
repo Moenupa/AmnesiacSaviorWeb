a11ec42d0e510f0f8bb223ce63d716d439d2cfbb
Moenupa WANG <mineworldy@outlook.com>
2021-04-29 10:59:43 +0800

    meta update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,15 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'AmnesiacSaviorWeb',
+    title: 'AmnesiacSavior - Plan Important Events with a Click',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Todo-list in the Cloud, Showing your tasks day by day' },
+      { hid: 'og:image', property: 'og:image', content: 'Amnesiac_Savior.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/angel.ico' }
