43b5c436b91ca928bfc073150a5c8bcb10225263
Moenupa WANG <mineworldy@outlook.com>
2021-04-29 12:49:44 +0800

    minor fixes

--- pages/index.vue
+++ pages/index.vue
@@ -163,7 +163,7 @@ export default {
 		},
     taskChecked() {
 			for (var i = 0; i < this.tasks.length; i++)
-				if (this.tasks[i].finished && this.tasks[i].targetDate === curTargetDate.getTime())
+				if (this.tasks[i].finished && this.tasks[i].targetDate === this.curTargetDate.getTime())
           return true;
 		},
     isTargetDate(index) {
